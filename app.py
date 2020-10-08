bc16a15b114fb7b3a4221eb1436b415541f5ce16
Habeeb Shopeju <shopejuh@gmail.com>
2020-10-08 14:03:34 +0100

    Update

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def load_chrome_driver():
       options.add_argument('--disable-gpu')
       options.add_argument('--no-sandbox')
       options.add_argument('--remote-debugging-port=9222')
-      
+      options.add_argument("--example-flag")
+
 
       return webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=options)
 
