9b3c6fbcfb8587cf0d79f6d2d2dc766158b5ffd7
Habeeb Shopeju <shopejuh@gmail.com>
2020-10-08 16:37:32 +0100

    Adding a worker

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 import redis as r
 from rq import Queue
+from worker import conn
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis = r.from_url(redis_url)
 
-q = Queue(connection=redis)
+q = Queue(connection=conn)
 
---
+++ worker.py
@@ -0,0 +1,13 @@
+import os
+
+from rq import Worker, Queue, Connection
+
+from config import redis
+
+listen = ['high', 'default', 'low']
+
+
+if __name__ == '__main__':
+    with Connection(redis):
+        worker = Worker(map(Queue, listen))
+        worker.work()
